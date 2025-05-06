949cf6852c91a82d615c43b55ad75c31a458b0ac
Javier Trujillo <xTrujix@gmail.com>
2025-05-06 22:16:52 +0200

    update file vercel

--- server/src/controllers/cvController.mjs
+++ server/src/controllers/cvController.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 export default function downloadCV(req, res) {
   try {
-    const filePath = path.join(__dirname, '../../public', 'CV.pdf');
+    const filePath = path.join(__dirname, '../../public', 'cv.pdf');
 
     fs.stat(filePath, (err, stats) => {
       if (err) {
@@ -22,7 +22,7 @@ export default function downloadCV(req, res) {
 
       console.log("File exists. Starting download...");
 
-      res.setHeader("Content-Disposition", 'attachment; filename="CV.pdf"');
+      res.setHeader("Content-Disposition", 'attachment; filename="cv.pdf"');
       res.setHeader("Content-Type", "application/pdf");
 
       res.sendFile(filePath);
