c75812a61f5915267bc5802bf901bf22e465e6de
Javier Trujillo <xTrujix@gmail.com>
2025-05-10 18:45:48 +0200

    update for render

--- server/src/controllers/cvController.mjs
+++ server/src/controllers/cvController.mjs
@@ -1,3 +1,4 @@
+// server/controllers/cvController.mjs
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
@@ -6,29 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default function downloadCV(req, res) {
-  try {
-    const filePath = path.join(__dirname, '../public', 'cv.pdf');
-
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        console.error("Error file not found:", err);
-        return res.status(404).send("File not found");
-      }
-
-      if (!stats.isFile()) {
-        console.log("File does not exist at specified path");
-        return res.status(404).send("File not found");
-      }
-
-      console.log("File exists. Starting download...");
-
-      res.setHeader("Content-Disposition", 'attachment; filename="javierTrujilloCV.pdf"');
-      res.setHeader("Content-Type", "application/pdf");
-
-      res.sendFile(filePath);
-    });
-  } catch (error) {
-    console.error("Error downloading file:", error);
-    return res.status(500).send("Error downloading file");
-  }
+  const filePath = path.join(__dirname, "../public", "cv.pdf");
+  fs.stat(filePath, (err, stats) => {
+    if (err || !stats.isFile()) {
+      return res.status(404).send("Archivo no encontrado");
+    }
+    res.setHeader(
+      "Content-Disposition",
+      'attachment; filename="javierTrujilloCV.pdf"'
+    );
+    res.setHeader("Content-Type", "application/pdf");
+    res.sendFile(filePath);
+  });
 }
